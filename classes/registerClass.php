78d723d0433b16597299b02bf915e1cae409659d
Abdulqadir Mohamedali <118472342+Abdulqadir53Mohamedali@users.noreply.github.com>
2024-04-22 09:59:26 +0100

    update

--- classes/registerClass.php
+++ classes/registerClass.php
@@ -105,3 +105,34 @@ class register extends database {
 
 
 ?>
+
+<!-- 
+
+Refactor your connect method to be static as well, then you can use self::connect() instead of $this->connect().
+
+If making connect static is not an option, you can indeed create an object within the static method and then use that object to call connect, like so:
+
+
+
+
+
+
+ -->
+
+<!-- 
+    In PHP, an expression is anything that has a value. For example:
+
+"$a + $b" is an expression whose value is the sum of $a and $b.
+A function call that returns a value, like myFunction(), is also an expression.
+The isset() function is meant to be used with variables, not with the result of expressions. 
+For example, isset($a) is valid, but isset($a + $b) is not because isset() cannot determine
+ if the result of $a + $b is set.
+ -->
+
+ <!-- 
+    If you're getting an "expression" error, it might be because of something else in the 
+    surrounding code. Double-check that you're not using isset() on an actual expression 
+    somewhere else. If the line you provided is what's causing the error, 
+    ensure that there are no syntax errors and that $_POST['submitBtn'] 
+    is indeed a variable that can be checked with isset().
+  -->
